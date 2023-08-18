ff48bb60aeb50be8858baa1a66a8e91cfc2582ac
Baek Gi Chan <laphayen@gmail.com>
2023-08-18 17:05:23 +0900

    Upload baek.2493.py

---
+++ baek.2493.py
@@ -0,0 +1,25 @@
+
+# 6 9 5 7 4
+# 수신한 탑 순서 번호의 인덱스
+
+# 입력부
+import sys
+n = int(sys.stdin.readline())
+stack = list(sys.stdin.readline().split())
+
+# 결과값 출력 배열
+result = []
+
+last = stack[-1]
+
+print(stack)
+
+for i in stack[-1::-1]:
+    if i <
+
+for i in result:
+    print(i)
+
+# for i in stack[-1::-1]:
+#     now = stack.pop()
+#     if now < last:
